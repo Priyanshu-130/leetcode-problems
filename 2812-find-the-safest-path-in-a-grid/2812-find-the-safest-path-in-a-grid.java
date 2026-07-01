686a09b21662f80684e455c13ed771c08cb49526
Priyanshu Raj <priyanshu.2427030305@muj.manipal.edu>
2026-07-01 20:22:00 +0530

    Time: 131 ms (73.33%), Space: 124 MB (29.52%) - LeetHub

---
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
@@ -0,0 +1,71 @@
+class Solution {
+    public int maximumSafenessFactor(List<List<Integer>> grid) {
+        int n = grid.size();
+        int[][] dist = new int[n][n];
+
+        for (int[] row : dist)
+            Arrays.fill(row, -1);
+
+        Queue<int[]> q = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid.get(i).get(j) == 1) {
+                    dist[i][j] = 0;
+                    q.offer(new int[]{i, j});
+                }
+            }
+        }
+
+        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+
+            for (int[] d : dir) {
+                int x = cur[0] + d[0];
+                int y = cur[1] + d[1];
+
+                if (x >= 0 && y >= 0 && x < n && y < n && dist[x][y] == -1) {
+                    dist[x][y] = dist[cur[0]][cur[1]] + 1;
+                    q.offer(new int[]{x, y});
+                }
+            }
+        }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a, b) -> b[2] - a[2]
+        );
+
+        boolean[][] vis = new boolean[n][n];
+        pq.offer(new int[]{0, 0, dist[0][0]});
+        vis[0][0] = true;
+
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int x = cur[0];
+            int y = cur[1];
+            int safe = cur[2];
+
+            if (x == n - 1 && y == n - 1)
+                return safe;
+
+            for (int[] d : dir) {
+                int nx = x + d[0];
+                int ny = y + d[1];
+
+                if (nx >= 0 && ny >= 0 && nx < n && ny < n && !vis[nx][ny]) {
+                    vis[nx][ny] = true;
+                    pq.offer(new int[]{
+                        nx,
+                        ny,
+                        Math.min(safe, dist[nx][ny])
+                    });
+                }
+            }
+        }
+
+        return 0;
+    }
+}
+    
